8fb8b375c5180b46deb5e38fc05bfcb3af050d83
ellesent <hypahflipperz@gmail.com>
2021-11-14 19:43:34 -0800

    drawer opening and closing

---
+++ Bug Police/Assets/Scripts/Nightstand.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nightstand : MonoBehaviour
+{
+       private bool isOpen = false;
+
+       private float toMove = 0.5f;
+
+    [SerializeField]
+    GameObject drawer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Vector3 currDrawerPosition = drawer.transform.position;
+        // Open/Close fridge door depending on state
+        // TODO Possibly move to a state machine
+        if (isOpen) {
+            drawer.transform.position = new Vector3(currDrawerPosition.x,  currDrawerPosition.y, currDrawerPosition.z + toMove);
+            isOpen=false;
+        }
+
+        else {
+            drawer.transform.position = new Vector3(currDrawerPosition.x, currDrawerPosition.y, currDrawerPosition.z - toMove);
+            isOpen=true;
+        }
+
+        Debug.Log("Nightstand Clicked!");
+    }
+}
