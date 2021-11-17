df283b9e522a37b72f6fbaa08252d213d028e339
ellesent <hypahflipperz@gmail.com>
2021-11-16 19:21:19 -0800

    working on main menu + messing with baked lighting

---
+++ Bug Police/Assets/Scripts/UI/CustomSceneManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CustomSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+      public void LoadScene(string scenename)
+    {
+        Debug.Log("sceneName to load: " + scenename);
+        SceneManager.LoadScene(scenename);
+    }
+}
